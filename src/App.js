33ef753558eef9748d4309afdc00789cd83ceec5
AimanK <akaya005@fiu.edu>
2023-09-09 20:33:05 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./layout/Navbar.js"
+import CustomerHome from "./pages/CustomerHome";
+import ProductHome from "./pages/ProductHome";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AddCustomer from "./customers/AddCustomer";
+import ViewCustomer from './customers/ViewCustomer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Navbar />
+
+      <Routes>
+          <Route exact path="/" element={< ProductHome />} />
+          <Route exact path="/customerhome" element={<CustomerHome />} />
+          <Route exact path="/addcustomer" element={<AddCustomer />} />
+          <Route exact path="/customer/:id" element={<ViewCustomer />} />
+      </Routes>
+
+      </Router>
     </div>
   );
 }
