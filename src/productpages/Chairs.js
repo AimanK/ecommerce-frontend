60084ee5e1a000ec49c0fbc2167c7fedfe3303ea
AimanK <akaya005@fiu.edu>
2023-09-17 13:26:19 -0400

    Add intial product page components and images

---
+++ src/productpages/Chairs.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Footer from '../layout/Footer.js';
+import Card from '../layout/Card.js';
+
+function Chairs() {
+  return (
+    <div class="container mt-4">
+    <div class="d-flex flex-row flex-wrap">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+     </div>
+
+            <Footer />
+    </div>
+
+  )
+}
+
+export default Chairs
